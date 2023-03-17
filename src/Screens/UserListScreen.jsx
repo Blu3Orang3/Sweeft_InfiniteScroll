ef47a665dafc909cb597c972737761b818fc5c9a
BlueOrang3 <matealtunashvili@gmail.com>
2023-03-18 01:47:26 +0400

    feat: initial screen

--- src/Screens/UserListScreen.jsx
+++ src/Screens/UserListScreen.jsx
@@ -1,6 +1,53 @@
+import { useState, useRef, useCallback } from 'react';
+import ProfileCard from '../Components/ProfileCard';
+import useList from '../hooks/useList';
+import LoadingSpinner from '../Components/LoadingSpinner';
+
 const UserListScreen = () => {
+  const [page, setPage] = useState(1);
+  const [size, setSize] = useState(8);
+  const { isLoading, isError, error, results, hasNextPage } = useList(
+    page,
+    size
+  );
+
+  const intObserver = useRef();
+  const lastPostRef = useCallback(
+    (user) => {
+      if (isLoading) return;
+
+      if (intObserver.current) intObserver.current.disconnect();
+
+      intObserver.current = new IntersectionObserver((users) => {
+        if (users[0].isIntersecting && hasNextPage) {
+          setPage((prev) => prev + 1);
+        }
+      });
+
+      if (user) intObserver.current.observe(user);
+    },
+    [isLoading, hasNextPage]
+  );
+
+  if (isError) return <p className='center'>Error: {error.message}</p>;
+  console.log(size, page, 'userlistscreen');
+
+  const content = results.map((user, i) => {
+    if (results.length === i + 1) {
+      return <ProfileCard ref={lastPostRef} key={user.id} user={user} />;
+    }
+    return <ProfileCard key={user.id} user={user} i={i} />;
+  });
+
   return (
-    <div>UserListScreen</div>
-  )
-}
-export default UserListScreen
+    <>
+      <div className='list-grid-container'> {content}</div>
+
+      { <LoadingSpinner />}
+      <p className='center'>
+        <a href='#top'>Back To Top</a>
+      </p>
+    </>
+  );
+};
+export default UserListScreen;
